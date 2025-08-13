e6ebf1e7ce2a08187d8076310d4c4d7ee5666405
Igor Ujiie <igorkazuhico@hotmail.com>
2025-08-13 16:09:29 -0300

    subindo projeto em branco

---
+++ src/main/java/com/meli/teamboardingBot/TeamboardingBotApplication.java
@@ -0,0 +1,13 @@
+package com.meli.teamboardingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamboardingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamboardingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/teamboardingBot/TeamboardingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.teamboardingBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamboardingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
