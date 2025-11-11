33ba4315db92ad153c43b7147e3a72a2d3f58a9a
develohome <devhome915@gmail.com>
2025-11-11 15:48:43 -0300

    atualização nas variáveis de comando

--- src/main/java/com/meli/teamboardingBot/service/command/SlashCommandRegister.java
+++ src/main/java/com/meli/teamboardingBot/service/command/SlashCommandRegister.java
@@ -1,21 +1,29 @@
 package com.meli.teamboardingBot.service.command;
+import com.meli.teamboardingBot.model.FormState;
 import jakarta.annotation.PostConstruct;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 @Component
 public class SlashCommandRegister {
     @Autowired
     private JDA jda;
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private FormState formState;
+
     @Value("${discord.guild.id}")
     private String guildId;
     @PostConstruct
     public void registerCommands() {
         jda.getGuildById(guildId).updateCommands().addCommands(
                 Commands.slash("squad-log", "Squad Log"),
-                Commands.slash("squad-log-lote", "Criar múltiplos squad logs de uma vez usando texto livre")
+                Commands.slash("squad-log-lote", messageSource.getMessage("txt_criar_multiplos_squad_logs_de_uma_vez_usando_texto_livre", null, formState.getLocale()))
         ).queue();
     }
 }
--- src/main/java/com/meli/teamboardingBot/service/command/SquadLogCommand.java
+++ src/main/java/com/meli/teamboardingBot/service/command/SquadLogCommand.java
@@ -1,10 +1,19 @@
 package com.meli.teamboardingBot.service.command;
+import com.meli.teamboardingBot.model.FormState;
 import com.meli.teamboardingBot.ui.Ui;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 @Component
 public class SquadLogCommand implements SlashCommandHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private FormState formState;
     @Override
     public String getName() {
         return "squad-log";
@@ -13,10 +22,10 @@ public class SquadLogCommand implements SlashCommandHandler {
     public void execute(SlashCommandInteractionEvent event) {
         event.deferReply(true).queue(hook ->
                 hook.editOriginalEmbeds(
-                        Ui.info("Escolha uma opção").build()
+                        Ui.info(messageSource.getMessage("txt_escolha_uma_opcao", null, formState.getLocale())).build()
                 ).setActionRow(
-                        Button.success("criar", "Criar"),
-                        Button.secondary("atualizar", "Atualizar")
+                        Button.success("criar",  messageSource.getMessage("txt_criar", null, formState.getLocale())),
+                        Button.secondary("atualizar", messageSource.getMessage("txt_atualizar", null, formState.getLocale()))
                 ).queue()
         );
     }
